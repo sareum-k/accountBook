43df1baad347486af9abe4a029e2b55398f324bf
sareum <rkdtkfma@gmail.com>
2022-07-17 15:48:11 +0900

    client setting 추가

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <div class="text-red-400 font-bold">hello!!!</div>
+  </div>
+</template>
+
+<style scoped>
+</style>
