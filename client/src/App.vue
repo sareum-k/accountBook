2fd688ce9806f1f400e27582fd28a74c60b0b4ea
sareum <rkdtkfma@gmail.com>
2022-08-31 23:16:09 +0900

    Apollo Client 재설정

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import gql from 'graphql-tag'
+// import { useQuery } from '@vue/apollo-composable'
+
+// const characters = gql`
+//   query {
+//     characters(page: 2, filter: { name: "rick" }) {
+//       info {
+//         count
+//       }
+//       results {
+//         name
+//       }
+//     }
+//     location(id: 1) {
+//       id
+//     }
+//     episodesByIds(ids: [1, 2]) {
+//       id
+//     }
+//   }
+// `
+
+// const { result, loading, error } = useQuery(characters)
+</script>
 
 <template>
   <div>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,22 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import './style.css'
 import App from './App.vue'
 import router from './router/router'
 
-createApp(App).use(router).mount('#app')
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:5173',
+  // uri: 'https://rickandmortyapi.com/graphql/',
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.use(router).mount('#app')
