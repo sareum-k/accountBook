08f26dee7e1373c2d8398dd94d14fc8c842bc7f2
sareum <rkdtkfma@gmail.com>
2022-09-14 22:17:23 +0900

    weekly list 추가

---
+++ client/src/components/row/MiddleRow.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    currentMonth: number
+    weekDate: number[]
+    weekWithdraw: number
+    weekDeposit: number
+  }>(),
+  {
+    weekWithdraw: 0,
+    weekDeposit: 0,
+  }
+)
+</script>
+<template>
+  <div
+    class="flex justify-between items-center border border-green-500 py-2 px-5 rounded-6"
+  >
+    <div class="text-sm font-semibold">
+      {{ currentMonth + 1 }}월 {{ weekDate[0] }}일 - {{ currentMonth + 1 }}월
+      {{ weekDate[weekDate.length - 1] }}일 ({{ weekDate.length }}일)
+    </div>
+    <div>
+      <div class="text-xs text-blue-500">
+        입금:
+        {{ weekDeposit.toLocaleString() ? weekDeposit.toLocaleString() : 0 }}원
+      </div>
+      <div class="text-xs text-red-500">
+        출금:
+        {{
+          weekWithdraw.toLocaleString() ? weekWithdraw.toLocaleString() : 0
+        }}원
+      </div>
+    </div>
+  </div>
+</template>
--- client/src/pages/accountBook/indexWeekly.vue
+++ client/src/pages/accountBook/indexWeekly.vue
@@ -1,4 +1,161 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import IconArrow from '@/components/icon/IconArrow.vue'
+import MiddleRow from '@/components/row/MiddleRow.vue'
+
+export interface ListItem {
+  startDate: number
+  endDate: number
+  dayCount: number
+  weekDeposit: number | null
+  weekWithdraw: number | null
+}
+
+const date = new Date()
+const utc = date.getTime() + date.getTimezoneOffset() * 60 * 1000
+const kstGap = 9 * 60 * 60 * 1000
+const today = new Date(utc + kstGap)
+const thisMonth = ref<Date>(
+  new Date(today.getFullYear(), today.getMonth(), today.getDate())
+)
+
+const currentYear = ref<number>(thisMonth.value.getFullYear())
+const currentMonth = ref<number>(thisMonth.value.getMonth())
+
+const movePrevMonth = () => {
+  thisMonth.value = new Date(currentYear.value, currentMonth.value - 1, 1)
+  currentYear.value = thisMonth.value.getFullYear()
+  currentMonth.value = thisMonth.value.getMonth()
+  getWeekDate()
+}
+
+const moveNextMonth = () => {
+  thisMonth.value = new Date(currentYear.value, currentMonth.value + 1, 1)
+  currentYear.value = thisMonth.value.getFullYear()
+  currentMonth.value = thisMonth.value.getMonth()
+  getWeekDate()
+  console.log(firstWeek.value)
+}
+
+/////////////////주간 날짜
+const firstWeek = ref<number[]>([])
+const secondWeek = ref<number[]>([])
+const thirdWeek = ref<number[]>([])
+const fourthWeek = ref<number[]>([])
+const fifthWeek = ref<number[]>([])
+const sixthWeek = ref<number[]>([])
+
+const getWeekDate = () => {
+  firstWeek.value = []
+  secondWeek.value = []
+  thirdWeek.value = []
+  fourthWeek.value = []
+  fifthWeek.value = []
+  sixthWeek.value = []
+
+  // 이전 달의 첫째 날 날짜와 요일 구하기
+  let startDay = new Date(currentYear.value, currentMonth.value, 0)
+  let prevDay = startDay.getDay()
+
+  // 이번 달의 마지막 날 날짜와 요일 구하기
+  let endDay = new Date(currentYear.value, currentMonth.value + 1, 0)
+  let nextDate = endDay.getDate() // 30
+
+  let weekDay = 1
+
+  for (let i = prevDay; i < 7; i++) {
+    firstWeek.value.push(weekDay++)
+  }
+  for (let i = 0; i < 7; i++) {
+    secondWeek.value.push(weekDay++)
+  }
+  for (let i = 0; i < 7; i++) {
+    thirdWeek.value.push(weekDay++)
+  }
+  for (let i = 0; i < 7; i++) {
+    fourthWeek.value.push(weekDay++)
+  }
+  for (let i = 0; i < 7; i++) {
+    if (nextDate >= weekDay) {
+      fifthWeek.value.push(weekDay++)
+    }
+  }
+  for (let i = 0; i < 7; i++) {
+    if (nextDate >= weekDay) {
+      sixthWeek.value.push(weekDay++)
+    }
+  }
+}
+
+getWeekDate()
+</script>
 <template>
-  <div>AccountBook</div>
+  <div class="flex justify-evenly">
+    <div class="w-1/2 space-y-4">
+      <div class="flex items-center justify-between">
+        <RouterLink :to="'/accountbook/create/indexCreate'">
+          <button
+            class="bg-green-900 text-yellow-500 text-sm px-4 py-1 rounded-6"
+          >
+            추가
+          </button>
+        </RouterLink>
+        <div class="flex space-x-8">
+          <button type="button" @click="movePrevMonth">
+            <IconArrow />
+          </button>
+          <div class="font-bold text-lg w-28 text-center">
+            {{ currentYear }}년 {{ currentMonth + 1 }}월
+          </div>
+          <button>
+            <IconArrow
+              class="rotate-180"
+              type="button"
+              @click="moveNextMonth"
+            />
+          </button>
+        </div>
+      </div>
+      <MiddleRow
+        :current-month="currentMonth"
+        :week-date="firstWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+      <MiddleRow
+        :current-month="currentMonth"
+        :week-date="secondWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+      <MiddleRow
+        :current-month="currentMonth"
+        :week-date="thirdWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+      <MiddleRow
+        :current-month="currentMonth"
+        :week-date="fourthWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+      <MiddleRow
+        v-if="fifthWeek.length !== 0"
+        :current-month="currentMonth"
+        :week-date="fifthWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+      <MiddleRow
+        v-if="sixthWeek.length !== 0"
+        :current-month="currentMonth"
+        :week-date="sixthWeek"
+        :week-withdraw="0"
+        :week-deposit="0"
+      />
+    </div>
+    <div class="w-1/3">Chart</div>
+  </div>
 </template>
